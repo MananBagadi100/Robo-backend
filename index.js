cf19e639e5db667fb6c98172f9565be0b01ca3f8
MananBagadi100 <mananbagadi100@gmail.com>
2025-12-17 19:26:08 +0530

    Added rate limiter to prevent api abuse and spamming

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Rate Limiter
+const rateLimit = require('express-rate-limit')
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 60 minutes
+	limit: 1, // Limit each IP to 2 requests per `window` (here, per 60 minutes).
+	standardHeaders: 'draft-8',
+	legacyHeaders: false,
+    handler : (req,res) => {
+        console.log('The IP of the client is ',req.headers['x-forwarded-for'] || req.connection.remoteAddress)
+        res.status(429).json({msg : 'Usage Limit Reached . Please try again after an hour'})
+    }
+})
+app.use(limiter)    //apply to all requests 
+//Tesing 
+app.use('/api/test',(req,res) => {
+    res.json({msg : "Hello from server !"})
+})
 // Routes
 app.use("/api/ai", aiRoutes);
 
